05dedcae69d3bee2a26c688927248ed5711186cb
Holden Karau <holden@pigscanfly.ca>
2019-01-12 14:34:39 -0800

    Add IBM subtree

--- update_subtree.sh
+++ update_subtree.sh
@@ -1,1 +1,2 @@
 git subtree pull  --prefix=examples git@github.com:intro-to-ml-with-kubeflow/intro-to-ml-with-kubeflow-examples.git master
+git subtree pull --prefix ibm-install-guide git@github.com:intro-to-ml-with-kubeflow/ibm-install-guide.git master
