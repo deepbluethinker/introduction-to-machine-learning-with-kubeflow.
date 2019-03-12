3380cb70d747baac8bf69bd4e52c0304d7716324
Trevor a.k.a @rawkintrevo <trevor.d.grant@gmail.com>
2019-03-12 08:52:33 -0500

    ch2 updates

--- examples/ch2_seldon_examples/setup_example.sh
+++ examples/ch2_seldon_examples/setup_example.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #tag::generate_kf_app[]
 # Platform can be one of: minikube, gcp, docker-for-desktop, ack, or null/none/generic
 kfctl.sh init hello_kubeflow --platform none
@@ -20,22 +21,18 @@ pushd ks_app
 
 #tag::setup_components[]
 # Set up Helm
-kubectl -n kube-system create sa tiller #already existed
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller #aldready existed
+kubectl -n kube-system create sa tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
 kubectl rollout status deploy/tiller-deploy -n kube-system
 # Setup SAD
-helm install seldon-core-analytics --name seldon-core-analytics --set grafana_prom_admin_password=password --set persistence.enabled=false --repo https://storage.googleapis.com/seldon-charts --namespace kubeflow
+helm install seldon-core-analytics \
+	--name seldon-core-analytics \
+	--set grafana_prom_admin_password=password \
+	--set persistence.enabled=false \
+	--repo https://storage.googleapis.com/seldon-charts \
+	--namespace kubeflow
 #end::setup_components[]
-
-
-## TODO check if nfs-1 exists before trying to create
-#tag::createPV[]
-export NAMESPACE=kubeflow
-# TODO change these to oreilly target
-kubectl create -f https://raw.githubusercontent.com/rawkintrevo/intro-to-ml-with-kubeflow-examples/master/ch2_seldon_examples/pv-volume.yaml -n $NAMESPACE
-kubectl create -f https://raw.githubusercontent.com/rawkintrevo/intro-to-ml-with-kubeflow-examples/master/ch2_seldon_examples/pv-claim.yaml -n $NAMESPACE
-#end:createPV[]
 
 # TODO(trevor): what version/tag?
 #tag::cloneSeldonExample[]
@@ -43,3 +40,13 @@ kubectl create -f https://raw.githubusercontent.com/rawkintrevo/intro-to-ml-with
 cd ~/
 git clone https://github.com/kubeflow/example-seldon
 #end::cloneSeldonExample[]
+
+## TODO check if nfs-1 exists before trying to create
+#tag::createPV[]
+export NAMESPACE=kubeflow
+# TODO change these to oreilly target
+kubectl create -f https://raw.githubusercontent.com/rawkintrevo/intro-to-ml-with-kubeflow-examples/master/ch2_seldon_examples/pv-volume.yaml -n $NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/rawkintrevo/intro-to-ml-with-kubeflow-examples/master/ch2_seldon_examples/pv-claim.yaml -n $NAMESPACE
+#end::createPV[]
+
+
